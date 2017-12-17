d087208cfa69874e3d8ed0b2fa9ab5c05f033617
Yota Toyama <raviqqe@gmail.com>
2017-12-17 17:23:47 +0900

    Exclude files by Jekyll's configuration

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -3,11 +3,5 @@ module.exports = {
   'globPatterns': [
     '**/*.{png,html,json}'
   ],
-  'swDest': 'service-worker.js',
-  'globIgnores': [
-    '../workbox-cli-config.js',
-    'node_modules',
-    'package.json',
-    'package-lock.json'
-  ]
+  'swDest': 'service-worker.js'
 }
