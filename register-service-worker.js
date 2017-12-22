99d2c9eb5690da4eb019ca0e44b67dbb8c5e632d
Yota Toyama <raviqqe@gmail.com>
2017-12-22 19:18:23 +0900

    Simplify service worker registerer script

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,5 +1,3 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then((reg) => {
-    console.log('Registration succeeded:', reg.scope)
-  }).catch(console.error)
+  navigator.serviceWorker.register('/service-worker.js')
 }
