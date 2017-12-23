c839f6c1439e61b3521b5aa203d0db6cf156556c
Yota Toyama <raviqqe@gmail.com>
2017-12-24 05:19:28 +0900

    Run linter

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import hljs = require("highlight.js");
 import $ = require("cash-dom");
+import hljs = require("highlight.js");
 
 import "./index.scss";
 
-hljs.initHighlightingOnLoad()
+hljs.initHighlightingOnLoad();
 
-$('a[href^="http://"], a[href^="https://"]').attr('target', '_blank')
+$('a[href^="http://"], a[href^="https://"]').attr("target", "_blank");
 
-$('a').on('mouseover', function() {
-    $('head').append($('<link/>').attr({ rel: 'prerender', href: $(this).attr('href') }))
-})
+$("a").on("mouseover", function() {
+    $("head").append($("<link/>").attr({ rel: "prerender", href: $(this).attr("href") }));
+});
