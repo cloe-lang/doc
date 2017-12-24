7dc8afe7756d83f0e73d288e02f73e62afd5ffbb
Yota Toyama <raviqqe@gmail.com>
2017-12-25 04:23:41 +0900

    Fix workbox config of directories

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,4 +1,16 @@
+const glob = require('glob')
+const path = require('path')
+
 module.exports = {
+  templatedUrls: Object.assign(...glob.sync('_site/**/*.html').map((filename) => {
+    const dirname = path.dirname(filename).slice(5)
+
+    if (path.basename(filename) === 'index.html' && dirname !== '') {
+      return {[dirname]: filename}
+    }
+
+    return {}
+  })),
   globDirectory: '_site',
   globPatterns: [
     '**/*'
