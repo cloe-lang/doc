c514ef3053cdcd9e0329c395fde355456deee51d
Yota Toyama <raviqqe@gmail.com>
2017-12-25 03:52:23 +0900

    Uninstall highlight.js

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import $ = require("cash-dom");
-import hljs = require("highlight.js");
 
 import "./index.scss";
-
-hljs.initHighlightingOnLoad();
 
 $('a[href^="http://"], a[href^="https://"]').attr("target", "_blank");
 
