0afb9cf75cff33c093cd2fe2aff300e9139b4a53
Yota Toyama <raviqqe@gmail.com>
2017-12-24 10:01:14 +0900

    Make it responsive

--- index.ts
+++ index.ts
@@ -10,3 +10,9 @@ $('a[href^="http://"], a[href^="https://"]').attr("target", "_blank");
 $("a").on("mouseover", function() {
     $('head link[rel="prerender"]').attr("href", $(this).attr("href"));
 });
+
+$("div.menu-button").on("click", function() {
+    $(this).toggleClass("on");
+    $("div.toc").toggleClass("shown");
+    $("div.content").toggleClass("hidden");
+});
