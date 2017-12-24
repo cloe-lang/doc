35bb0bb3ac2015054830c91bf0e6c9db62eeff5f
Yota Toyama <raviqqe@gmail.com>
2017-12-25 08:10:57 +0900

    Fix style of modify-html script

--- bin/modify-html.ts
+++ bin/modify-html.ts
@@ -1,22 +1,23 @@
-const fs = require('fs')
-const jsdom = require('jsdom')
+import fs = require("fs");
+import jquery = require("jquery");
+import jsdom = require("jsdom");
 
 Promise.all(process.argv.slice(2).map((filename) => {
-  fs.readFile(filename, 'utf8', (error, data) => {
-    if (error) {
-      throw error
-    }
+    fs.readFile(filename, "utf8", (error, data) => {
+        if (error) {
+            throw error;
+        }
 
-    const { window } = new jsdom.JSDOM(data)
-    const $ = require('jquery')(window)
+        const { window } = new jsdom.JSDOM(data);
+        const $ = jquery(window);
 
-    $('a[href^="http://"], a[href^="https://"]').attr('target', '_blank')
-    $('code').addClass('highlight')
+        $('a[href^="http://"], a[href^="https://"]').attr("target", "_blank");
+        $("code").addClass("highlight");
 
-    fs.writeFile(filename, window.document.documentElement.outerHTML, (error) => {
-      if (error) {
-        throw error
-      }
-    })
-  })
-}))
+        fs.writeFile(filename, window.document.documentElement.outerHTML, (error) => {
+            if (error) {
+                throw error;
+            }
+        });
+    });
+}));
