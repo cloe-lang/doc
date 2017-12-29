b7e90fd0c0d0788a93cd6283e682ba92cca47373
Yota Toyama <raviqqe@gmail.com>
2017-12-30 05:30:07 +0900

    Use promisify

--- bin/modify-html.ts
+++ bin/modify-html.ts
@@ -1,34 +1,25 @@
 import fs = require("fs");
 import jquery = require("jquery");
 import jsdom = require("jsdom");
+import util = require("util");
 
-process.argv.slice(2).map((filename) => {
-    fs.readFile(filename, "utf8", (error, data) => {
-        if (error) {
-            throw error;
-        }
+process.argv.slice(2).map(async (filename) => {
+    const { window } = new jsdom.JSDOM(await util.promisify(fs.readFile)(filename, "utf8"));
+    const $ = jquery(window);
 
-        const { window } = new jsdom.JSDOM(data);
-        const $ = jquery(window);
-
-        $('a[href^="http://"], a[href^="https://"]').attr({
-            rel: "noopener", // Prevent tabnabbing.
-            target: "_blank",
-        });
-        $("code").addClass("highlight");
-        $("pre").removeClass("highlight");
-
-        const unwrap = function() {
-            $(this).parent().replaceWith($(this).children());
-        };
-
-        $("div.highlight").each(unwrap);
-        $("div.highlighter-rouge").each(unwrap);
-
-        fs.writeFile(filename, window.document.documentElement.outerHTML, (error) => {
-            if (error) {
-                throw error;
-            }
-        });
+    $('a[href^="http://"], a[href^="https://"]').attr({
+        rel: "noopener", // Prevent tabnabbing.
+        target: "_blank",
     });
+    $("code").addClass("highlight");
+    $("pre").removeClass("highlight");
+
+    const unwrap = function() {
+        $(this).parent().replaceWith($(this).children());
+    };
+
+    $("div.highlight").each(unwrap);
+    $("div.highlighter-rouge").each(unwrap);
+
+    await util.promisify(fs.writeFile)(filename, window.document.documentElement.outerHTML);
 });
