c0ad96cf5adc6d07c5406ba7317ad379a9af6c3a
Yota Toyama <raviqqe@gmail.com>
2018-03-26 15:47:36 +0900

    Format JavaScript scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,46 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
 const cssLoader = {
-  loader: 'css-loader',
-  options: { minimize: true }
-}
+	loader: 'css-loader',
+	options: {
+		minimize: true
+	}
+};
 
 module.exports = {
-  entry: './index.ts',
-  output: {
-    filename: 'tmp/index.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: cssLoader
-        })
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [cssLoader, { loader: 'sass-loader' }]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin('_includes/index.css')
-  ]
-}
+	entry: './index.ts',
+	output: {
+		filename: 'tmp/index.js'
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	module: {
+		rules: [{
+				test: /\.ts$/,
+				loader: 'ts-loader'
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: cssLoader
+				})
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [cssLoader, {
+						loader: 'sass-loader'
+					}]
+				})
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin(),
+		new ExtractTextPlugin('_includes/index.css')
+	]
+};
--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,28 +1,32 @@
-const glob = require('glob')
-const path = require('path')
+const glob = require('glob');
+const path = require('path');
 
-const indexFile = 'index.html'
+const indexFile = 'index.html';
 
 module.exports = {
-  templatedUrls: Object.assign(...glob.sync('_site/**/*.html').map((filename) => {
-    const dirname = path.dirname(filename).slice(6)
-    const basename = path.basename(filename)
+	templatedUrls: Object.assign(...glob.sync('_site/**/*.html').map((filename) => {
+		const dirname = path.dirname(filename).slice(6);
+		const basename = path.basename(filename);
 
-    if (basename === indexFile && dirname !== '') {
-      return {[dirname]: filename}
-    } else if (path.extname(filename) === '.html' && basename !== indexFile) {
-      return {[filename.slice(6, -5)]: filename}
-    }
+		if (basename === indexFile && dirname !== '') {
+			return {
+				[dirname]: filename
+			};
+		} else if (path.extname(filename) === '.html' && basename !== indexFile) {
+			return {
+				[filename.slice(6, -5)]: filename
+			};
+		}
 
-    return {}
-  })),
-  globDirectory: '_site',
-  globPatterns: [
-    '**/*'
-  ],
-  globIgnores: [
-    'register-service-worker.js',
-    '*.map'
-  ],
-  swDest: '_site/service-worker.js'
-}
+		return {};
+	})),
+	globDirectory: '_site',
+	globPatterns: [
+		'**/*'
+	],
+	globIgnores: [
+		'register-service-worker.js',
+		'*.map'
+	],
+	swDest: '_site/service-worker.js'
+};
