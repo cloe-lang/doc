b7d055402f1ce601375ce022053b0ee810f088ca
Yota Toyama <raviqqe@gmail.com>
2018-05-10 12:58:40 +0900

    Put icon as home button

--- rakefile.rb
+++ rakefile.rb
@@ -65,12 +65,16 @@ file '_includes/index.css' => 'tmp/index.js' do |t|
   cp t.source.ext('.css'), t.name
 end
 
-directory '_site' => '_includes/index.css' do
-  sh 'bundler exec jekyll build'
-end
-
 file 'tmp/icon.svg' => 'tmp' do |t|
   curl 'https://github.com/cloe-lang/icon/raw/master/icon.svg', t.name
+end
+
+file '_includes/icon.svg' => 'tmp/icon.svg' do |t|
+  cp t.source, t.name
+end
+
+directory '_site' => %w[_includes/index.css _includes/icon.svg] do
+  sh 'bundler exec jekyll build'
 end
 
 file '_site/index.js' => 'tmp/index.js' do |t|
