a40800b8460f2c98fe0330e39a6e218bb950bc70
Yota Toyama <raviqqe@gmail.com>
2018-06-27 21:58:51 +0900

    Remove script to handle clean URLs manually

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,25 +1,4 @@
-const glob = require('glob');
-const path = require('path');
-
-const indexFile = 'index.html';
-
 module.exports = {
-	templatedUrls: Object.assign(...glob.sync('_site/**/*.html').map((filename) => {
-		const dirname = path.dirname(filename).slice(6);
-		const basename = path.basename(filename);
-
-		if (basename === indexFile && dirname !== '') {
-			return {
-				[dirname]: filename
-			};
-		} else if (path.extname(filename) === '.html' && basename !== indexFile) {
-			return {
-				[filename.slice(6, -5)]: filename
-			};
-		}
-
-		return {};
-	})),
 	globDirectory: '_site',
 	globPatterns: ['**/*'],
 	swDest: '_site/service-worker.js'
