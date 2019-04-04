3e7da25ec5713fd12fcb3e7fd11fe6bd6e1de959
Yota Toyama <raviqqe@gmail.com>
2019-04-04 04:50:00 +0000

    Fix slow centering

--- index.ts
+++ index.ts
@@ -4,27 +4,26 @@ const toc = document.querySelector(".toc");
 const content = document.querySelector(".content");
 
 function centerToc() {
-    toc.querySelector(".current").scrollIntoView({ block: "center" });
+  toc.querySelector(".current").scrollIntoView({ block: "center" });
 }
 
 document.querySelector(".menu-button").addEventListener("click", function() {
-    this.classList.toggle("on");
-    toc.classList.toggle("hidden");
-    content.classList.toggle("hidden");
-    centerToc();
+  this.classList.toggle("on");
+  toc.classList.toggle("hidden");
+  content.classList.toggle("hidden");
+  centerToc();
 });
 
 centerToc();
-toc.classList.remove("invisible");
 
 const link = document.querySelector('link[rel="prerender"]');
 
 for (const element of Array.from(document.getElementsByTagName("a"))) {
-    element.addEventListener("mouseover", function() {
-        link.setAttribute("href", this.getAttribute("href"));
-    });
+  element.addEventListener("mouseover", function() {
+    link.setAttribute("href", this.getAttribute("href"));
+  });
 }
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js");
+  navigator.serviceWorker.register("/service-worker.js");
 }
