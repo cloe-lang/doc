fc8a173ca4db9771ae7deb2d1c9775d251e07adb
Yota Toyama <raviqqe@gmail.com>
2019-04-04 05:24:11 +0000

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,42 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-
-const cssLoader = {
-	loader: 'css-loader',
-	options: {
-		minimize: true
-	}
-};
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-	mode: 'production',
-	entry: './index.ts',
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'tmp')
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-	module: {
-		rules: [{
-				test: /\.ts$/,
-				loader: 'ts-loader',
-				options: {
-					configFile: require.resolve('./tsconfig.webpack.json')
-				}
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: cssLoader
-				})
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [cssLoader, {
-						loader: 'sass-loader'
-					}]
-				})
-			}
-		]
-	},
-	optimization: {
-		minimize: true
-	},
-	plugins: [
-		new ExtractTextPlugin('index.css')
-	]
+  mode: "production",
+  entry: "./index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "tmp")
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          configFile: require.resolve("./tsconfig.webpack.json")
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader"]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
+      }
+    ]
+  },
+  optimization: {
+    minimize: true
+  },
+  plugins: [new ExtractTextPlugin("index.css")]
 };
