5e074e3cf6452d2cf67617c30ed55c0644ca5e88
Yota Toyama <raviqqe@gmail.com>
2022-05-30 21:13:21 -0700

    Fix fonts (#21)
    * Fix font paths
    
    * Inline them
    
    * Refactor
    
    * Fix paths
    
    * Revert "Refactor"
    
    This reverts commit e32182154e40ca7ecb387f646bf7eec708dcd466.
    
    * Remove config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "tmp/webpack"),
+    publicPath: "/",
   },
   resolve: {
     extensions: [".ts", ".js"],
