0746f540edfb3b37dbe52a9769d50261d74c41f8
Yota Toyama <raviqqe@gmail.com>
2023-12-09 19:54:24 +0900

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
-require("webpack");
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { resolve } from "node:path";
+import "webpack";
 
-module.exports = {
+export default {
   mode: "production",
   entry: "./index.ts",
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "tmp/webpack"),
+    path: resolve("tmp/webpack"),
     publicPath: "/",
   },
   resolve: {
@@ -19,7 +19,7 @@ module.exports = {
         test: /\.ts$/,
         loader: "ts-loader",
         options: {
-          configFile: require.resolve("./tsconfig.webpack.json"),
+          configFile: resolve("./tsconfig.webpack.json"),
         },
       },
       {
