fe9c1d9a2f7a266cbec2dac66dc2e188885ec2c8
Yota Toyama <raviqqe@gmail.com>
2026-01-19 19:53:15 -0800

    Fix

--- rakefile.rb
+++ rakefile.rb
@@ -1,119 +1,120 @@
-require "uri"
+require 'uri'
 
 def curl(args, dest)
   sh "curl -fsSL '#{args}' > #{dest}"
 end
 
 task :init do
-  sh "npm install"
-  sh "bundle install"
+  sh 'pnpm i'
+  sh 'bundle install'
 end
 
-directory "tmp"
+directory 'tmp'
 
-{ text: "Noto Sans", code: "Roboto Mono" }.each do |kind, family|
-  file "tmp/#{kind}.css" => "tmp" do |t|
+{ text: 'Noto Sans', code: 'Roboto Mono' }.each do |kind, family|
+  file "tmp/#{kind}.css" => 'tmp' do |t|
     query = URI.encode_www_form(
       family: family,
-      text: (" ".."~").to_a.join,
+      text: (' '..'~').to_a.join
     )
 
     curl "https://fonts.googleapis.com/css?#{query}", t.name
   end
 end
 
-rule "-font.css" => ".css" do |t|
+rule '-font.css' => '.css' do |t|
   File.write(t.name, File.read(t.source)
     .sub(/font-family:[^;]*;/,
-         "font-family: #{t.source.pathmap("%n").capitalize}Font;
+         "font-family: #{t.source.pathmap('%n').capitalize}Font;
           font-display: swap;")
     .sub(/url\([^)]*\)/, t.source.pathmap('url("%n.woff2")'))
     .sub(/format\([^)]*\)/, 'format("woff2")'))
 end
 
-rule ".ttf" => ".css" do |t|
+rule '.ttf' => '.css' do |t|
   curl(/url\(([^)]+)\)/.match(File.read(t.source))[1], t.name)
 end
 
-rule %r{tmp/.*\.woff2} => ->(f) { f.pathmap("tmp/%n.ttf") } do |t|
+rule %r{tmp/.*\.woff2} => ->(f) { f.pathmap('tmp/%n.ttf') } do |t|
   sh "npx ttf2woff2 < #{t.source} > #{t.name}"
 end
 
-file "tmp/webpack/index.js" => %w[
-       tmp/text-font.css
-       tmp/code-font.css
-       tmp/text.woff2
-       tmp/code.woff2] do
-  sh "npx webpack-cli"
+file 'tmp/webpack/index.js' => %w[
+  tmp/text-font.css
+  tmp/code-font.css
+  tmp/text.woff2
+  tmp/code.woff2
+] do
+  sh 'npx webpack-cli'
 end
 
-file "tmp/webpack/main.css" => "tmp/webpack/index.js"
+file 'tmp/webpack/main.css' => 'tmp/webpack/index.js'
 
-file "_includes/index.css" => "tmp/webpack/main.css" do |t|
-  cp t.source.ext(".css"), t.name
+file '_includes/index.css' => 'tmp/webpack/main.css' do |t|
+  cp t.source.ext('.css'), t.name
 end
 
-file "_includes/icon.svg" do |t|
-  curl "https://github.com/cloe-lang/icon/raw/master/icon.svg", t.name
+file '_includes/icon.svg' do |t|
+  curl 'https://github.com/cloe-lang/icon/raw/master/icon.svg', t.name
 end
 
-file "_includes/x.svg" do |t|
+file '_includes/x.svg' do |t|
   curl(
-    "https://github.com/simple-icons/simple-icons/raw/master/icons/x.svg",
+    'https://github.com/simple-icons/simple-icons/raw/master/icons/x.svg',
     t.name
   )
 end
 
-directory "tmp/cloe" do |t|
+directory 'tmp/cloe' do |t|
   sh "git clone https://github.com/cloe-lang/cloe #{t.name}"
 end
 
-directory "examples" => "tmp/cloe" do |t|
-  sh "go run github.com/raviqqe/gherkin2markdown #{File.join t.source, "examples"} #{t.name}"
-  File.write(File.join(t.name, "index.md"),
+directory 'examples' => 'tmp/cloe' do |t|
+  sh "go run github.com/raviqqe/gherkin2markdown #{File.join t.source, 'examples'} #{t.name}"
+  File.write(File.join(t.name, 'index.md'),
              "# Examples\n\n" \
-             "Code examples which describes usage of the language features " \
-             "and built-in functions and modules.")
+             'Code examples which describes usage of the language features ' \
+             'and built-in functions and modules.')
 end
 
-directory "_site" => %w[
-            _includes/index.css
-            _includes/icon.svg
-            _includes/x.svg
-            examples
-          ] do
-  sh "bundle exec jekyll build"
+directory '_site' => %w[
+  _includes/index.css
+  _includes/icon.svg
+  _includes/x.svg
+  examples
+] do
+  sh 'bundle exec jekyll build'
 end
 
-file "_site/index.js" => "tmp/webpack/index.js" do |t|
+file '_site/index.js' => 'tmp/webpack/index.js' do |t|
   cp t.source, t.name
 end
 
-file "_site/icon.svg" => "_includes/icon.svg" do |t|
+file '_site/icon.svg' => '_includes/icon.svg' do |t|
   cp t.source, t.name
 end
 
 task build: %w[_site _site/index.js _site/icon.svg] do
-  sh "npx tsx bin/modify-html.ts #{Dir.glob("_site/**/*.html").join " "}"
-  sh "npx workbox generateSW workbox-cli-config.cjs"
-  cp Dir.glob("tmp/webpack/*.woff2"), "_site"
+  sh "npx tsx bin/modify-html.ts #{Dir.glob('_site/**/*.html').join ' '}"
+  sh 'npx workbox generateSW workbox-cli-config.cjs'
+  cp Dir.glob('tmp/webpack/*.woff2'), '_site'
 end
 
 task :deploy do
-  sh "npx firebase deploy"
+  sh 'npx firebase deploy'
 end
 
 task default: %w[build deploy]
 
 task run: :build do
-  sh "npx superstatic --debug"
+  sh 'npx superstatic --debug'
 end
 
 task :lint do
-  sh "rufo -c *.rb"
-  sh "npx stylelint **/*.scss"
+  sh 'rufo -c *.rb'
+  sh 'npx stylelint **/*.scss'
 end
 
 task :clean do
-  sh %w[git clean -dfx --exclude node_modules].join " "
+  sh %w[git clean -dfx --exclude node_modules].join ' '
 end
